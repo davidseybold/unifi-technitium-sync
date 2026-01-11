c367091dd2daba83313721c7b7e33b29b9bd1908
David Seybold <davidseybold@pm.me>
2026-01-11 13:41:53 -0500

    Moved to a provider model to allow supporting other providers (#6)

---
+++ dnsprovider/technitium/provider.go
@@ -0,0 +1,177 @@
+package technitium
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/caarlos0/env/v11"
+
+	"github.com/davidseybold/unifi-dns-sync/dnsprovider"
+)
+
+type Provider struct {
+	BaseURL    string
+	Token      string
+	HTTPClient *http.Client
+}
+
+type config struct {
+	BaseURL  string `env:"TECHNITIUM_API_URL"`
+	APIToken string `env:"TECHNITIUM_API_TOKEN"`
+}
+
+func (c *config) Validate() error {
+	if c.BaseURL == "" {
+		return errors.New("TECHNITIUM_API_URL is required for technitium provider")
+	}
+
+	if c.APIToken == "" {
+		return errors.New("TECHNITIUM_API_TOKEN is required for technitium provider")
+	}
+
+	return nil
+}
+
+func loadConfig() (config, error) {
+	var c config
+	err := env.Parse(&c)
+	return c, err
+}
+
+func NewProvider() (*Provider, error) {
+	cfg, err := loadConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+
+	return &Provider{
+		BaseURL: cfg.BaseURL,
+		Token:   cfg.APIToken,
+		HTTPClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}, nil
+}
+
+func (c *Provider) listZones(ctx context.Context) ([]Zone, error) {
+
+	response, err := doRequest[ListZonesResponse](ctx, c, "/api/zones/list", url.Values{})
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Zones, nil
+}
+
+// GetZone fetches details for a specific zone
+func (c *Provider) GetZone(ctx context.Context, zoneName string) (dnsprovider.Zone, error) {
+	zones, err := c.listZones(ctx)
+	if err != nil {
+		return dnsprovider.Zone{}, err
+	}
+
+	for i := range zones {
+		if zones[i].Name == zoneName {
+			return zones[i].ToDNSProviderZone(), nil
+		}
+	}
+
+	return dnsprovider.Zone{}, dnsprovider.ErrZoneNotFound
+}
+
+func (c *Provider) UpsertRecord(ctx context.Context, zone string, record dnsprovider.Record) error {
+	params := url.Values{}
+	params.Add("zone", zone)
+	params.Add("domain", record.Name)
+	params.Add("type", record.Type)
+	params.Add("ttl", fmt.Sprintf("%d", record.TTL))
+	params.Add("comments", record.Comments)
+	params.Add("ptr", "true")
+	params.Add("createPtrZone", "true")
+	params.Add("overwrite", "true")
+	params.Add("ipAddress", record.IPAddress)
+
+	_, err := doRequest[struct{}](ctx, c, "/api/zones/records/add", params)
+
+	return err
+}
+
+// ListRecords returns all records for a zone
+func (c *Provider) ListRecords(ctx context.Context, zone string) ([]dnsprovider.Record, error) {
+	params := url.Values{}
+	params.Add("zone", zone)
+	params.Add("domain", zone)
+	params.Add("listZone", "true")
+
+	records, err := doRequest[ListRecordsResponse](ctx, c, "/api/zones/records/get", params)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []dnsprovider.Record
+	for i := range records.Records {
+		result = append(result, records.Records[i].ToDNSProviderRecord())
+	}
+
+	return result, nil
+}
+
+// // DeleteRecord removes a record from a zone
+func (c *Provider) DeleteRecord(ctx context.Context, zone string, record dnsprovider.Record) error {
+	params := url.Values{}
+	params.Add("zone", zone)
+	params.Add("domain", record.Name)
+	params.Add("type", record.Type)
+	params.Add("ipAddress", record.IPAddress)
+
+	_, err := doRequest[struct{}](ctx, c, "/api/zones/records/delete", params)
+
+	return err
+}
+
+func doRequest[T any](ctx context.Context, c *Provider, path string, params url.Values) (*T, error) {
+	params.Add("token", c.Token)
+
+	fullURL := fmt.Sprintf("%s%s?%s", c.BaseURL, path, params.Encode())
+
+	req, err := http.NewRequestWithContext(ctx, "GET", fullURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("api error: %s (status: %d)", resp.Status, resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var apiResp APIResponse[T]
+	if err := json.Unmarshal(body, &apiResp); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal base response: %v", err)
+	}
+
+	if apiResp.Status != "ok" {
+		return nil, fmt.Errorf("api error: %s (status: %s)", apiResp.ErrorMessage, resp.Status)
+	}
+
+	return &apiResp.Response, nil
+}
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/davidseybold/unifi-technitium-sync
+module github.com/davidseybold/unifi-dns-sync
 
 go 1.24.2
 
